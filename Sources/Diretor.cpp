b9143c9b954b6581628243798afb65389a8e4042
Leandeson Pinheiro <leandersonpinheiro20@gmail.com>
2022-06-04 13:31:22 -0300

    Teste

--- Sources/Diretor.cpp
+++ Sources/Diretor.cpp
@@ -33,17 +33,3 @@ void Diretor::ExibirLista(){
  ExibirRegistro();
  }
 
-void Diretor::BuscaDiretor(string cod){
-  if(getNome().find(cod) != string ::npos ){
-    ExibirRegistro();
-  }else
-   cout << "Funcionario não cadastrado!";
-}
-
-void Diretor::ExibirRegistro(){
-cout << "-------------------------------------------------------------------------------------------------------------------" << endl;
-cout << "\n---Dados do Diretores: " <<Nome <<"\n" <<endl;
-cout <<"Nome:" << Nome <<" Código: "<< Codigo << " Endereço: " << Endereco << " Telefone: " << Telefone << endl;
-cout << "Designação: " << Designacao << " Data de Ingresso: " << Dia <<"/" << Mes <<"/"<<Ano << " Salário: " << Salario << endl;
-cout << "----------------------------------------------------------------------------------------------------------------------" << endl;
- }
